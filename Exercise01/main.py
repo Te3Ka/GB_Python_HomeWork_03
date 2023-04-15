25db46d75ebd9ded061ad25529aff8d6816baf22
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-04-15 14:22:44 +0300

    1 задание. Создана функция для генерации массива чисел

--- Exercise01/main.py
+++ Exercise01/main.py
@@ -26,3 +26,22 @@ def author():
     print('79811131773@yandex.ru')
     print('****************************')
 
+# Проверка на введение положительного числа.
+def correct_number():
+    _number = int(input("Введите количество элементов в массиве: "))
+    while (_number < 0):
+        _number = int(input("Введено неверное число! Введите заново: "))
+    return _number
+
+# Создание массива случайных чисел
+def generate_random_list(_min, _max, _num):
+    _list = []
+    for i in range(int(_num)):
+        _list.append(random.randint(_min, _max))
+    return _list
+
+print("Программа делает подсчёт заданного пользователем числа в массиве случайных чисел,\n" + 
+      "в котором количество элементов задаётся пользователем.")
+_num = correct_number()
+_list_rand_num = generate_random_list(0, 9, _num)
+print(_list_rand_num)
